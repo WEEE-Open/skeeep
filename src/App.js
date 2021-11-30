eff33ef6a0d8e9abb6ed66f015d0c07af2735ea5
Hesam Mashhadi Mohammad <s271199@studenti.polito.it>
2021-11-30 10:54:12 +0330

    auto dark mode detection adding custom css for dark mode

--- src/App.js
+++ src/App.js
@@ -1,16 +1,37 @@
-import Button from 'react-bootstrap/Button'
+import Button from "react-bootstrap/Button";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
-import CardsContainer from './Components/CardsContainer';
-import Footer from './Components/Footer';
-import Header from './Components/Header';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import CardsContainer from "./Components/CardsContainer";
+import Footer from "./Components/Footer";
+import Header from "./Components/Header";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [dark, setDark] = useState(false);
+
+  useEffect(() => {
+    let current = new Date();
+
+    let cTime = current.getHours();
+    if(cTime < 6 || cTime>20){
+      setDark(true)
+    }else{
+      setDark(false)
+    }
+  }, []);
+
+  useEffect(()=>{
+    if(dark){
+      document.body.classList.add("dark");
+    }else{
+      document.body.classList.remove("dark");
+    }
+    
+  },[dark])
+  
   return (
     <div className="App">
-  
-
       <Header></Header>
       <CardsContainer></CardsContainer>
       <Footer></Footer>
