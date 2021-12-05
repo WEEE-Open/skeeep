6a30a3b7d9b550a97679ba66de1aa55c54ac1e41
e-caste-mbp <git@caste.dev>
2021-12-05 10:58:13 +0100

    add English cards translation. ref #14

--- src/Components/Translations.js
+++ src/Components/Translations.js
@@ -1,17 +1,55 @@
-
-// translation for 'en' and 'it'
 const translations = {
 	"en-US": {
-		"text" : "Some quick example text to build on the card title and make up the bulk of the card's content.",
-		"footer" : "This is the footer"
+		"footer" : "This is the footer",
+		"inginfbot": {
+			"subtitle": "A Telegram bot to find other PoliTo Computer Engineers",
+			"description": "Using this bot you can easily find people from courses you're enrolled in, discuss exams " +
+				"and your common interests. All organized in managed groups.",
+		},
+		"politools": {
+			"subtitle": "A browser extension for PoliTo's portal page",
+			"description": "Add functionality to PoliTo's teaching portal and improve your everyday life. Totally " +
+				"free and student-developed.",
+		},
+		"politogruppibot": {
+			"subtitle": "A Telegram bot to find other PoliTo students",
+			"description": "Find people enrolled in your same courses or with common interests. Ask to add your own " +
+				"groups or find the existing managed ones. For any PoliTo student.",
+		},
+		"sonousciteledate": {
+			"subtitle": "Are the exam dates out yet?",
+			"description": "A single page website that tells you if exam dates are out, or when they are expected to " +
+				"be available. Frequently updated.",
+		},
+		"studyplanner": {
+			"subtitle": "A cross-platform desktop app to estimate the time needed to study for an exam",
+			"description": "Download this free open source desktop application to your Windows/Linux/macOS computer, " +
+				"select the slides you have to study and/or the video lectures you have to watch, and Study Planner " +
+				"will give you an estimate of time required to study them in terms of days.",
+		},
 	},
-	"en-GB": {
-		"text" : "Some quick example text to build on the card title and make up the bulk of the card's content.",
-		"footer" : "This is the footer"
-	},
-	"it":{
-		"text" : "Un testo di esempio rapido da costruire sul titolo della carta e costituire la maggior parte del contenuto della carta.",
-		"footer" : "questo è il piè di pagina"
+	"it": {
+		"footer" : "questo è il piè di pagina",
+		"inginfbot": {
+			"subtitle": "",
+			"description": "",
+		},
+		"politools": {
+			"subtitle": "",
+			"description": "",
+		},
+		"politogruppibot": {
+			"subtitle": "",
+			"description": "",
+		},
+		"sonousciteledate": {
+			"subtitle": "",
+			"description": "",
+		},
+		"studyplanner": {
+			"subtitle": "",
+			"description": "",
+		},
 	}
 };
 
