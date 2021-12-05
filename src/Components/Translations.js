7aeb00315d06aeb44d450bfe4a1c845edfa5d4bd
e-caste-mbp <git@caste.dev>
2021-12-05 11:08:00 +0100

    add Italian cards translation. ref #14

--- src/Components/Translations.js
+++ src/Components/Translations.js
@@ -31,24 +31,31 @@ const translations = {
 	"it": {
 		"footer" : "questo è il piè di pagina",
 		"inginfbot": {
-			"subtitle": "",
-			"description": "",
+			"subtitle": "Un bot Telegram per trovare altri Ingegneri Informatici del PoliTo",
+			"description": "Usando questo bot puoi trovare facilmente altri studenti dei corsi che segui, discutere " +
+				"di esami e dei vostri interessi comuni. Tutto organizzato in gruppi moderati.",
 		},
 		"politools": {
-			"subtitle": "",
-			"description": "",
+			"subtitle": "Un'estensione browser per il portale della didattica del PoliTo",
+			"description": "Aggiungi funzionalità al portale della didattica del PoliTo e migliora la tua vita " +
+				"quotidiana. Completamente gratuita e sviluppata da studenti.",
 		},
 		"politogruppibot": {
-			"subtitle": "",
-			"description": "",
+			"subtitle": "Un bot Telegram per trovare altri studenti del PoliTo",
+			"description": "Trova altri studenti iscritti ai tuoi stessi corsi o con cui condividi degli interessi. " +
+				"Richiedi di aggiungere il tuo gruppo o trova quelli già esistenti e moderati. Per qualsiasi " +
+				"studente del PoliTo.",
 		},
 		"sonousciteledate": {
-			"subtitle": "",
-			"description": "",
+			"subtitle": "Sono uscite le date degli esami?",
+			"description": "Una singola pagina web che ti dice se le date degli esami sono già uscite, o quando ci " +
+				"si aspetta che escano. Aggiornata di frequente.",
 		},
 		"studyplanner": {
-			"subtitle": "",
-			"description": "",
+			"subtitle": "Un'applicazione desktop cross-platform per stimare il tempo per studiare un esame",
+			"description": "Scarica questa applicazione desktop gratuita ed open source sul tuo computer Windows/" +
+				"Linux/macOS, seleziona le slide che devi studiare e/o le videolezioni che devi guardare, e Study " +
+				"Planner ti darà una stima di quanti giorni dovrai studiare per quell'esame.",
 		},
 	}
 };
