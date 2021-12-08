6b36d239765b4082aed8c015a0ee71e702241521
Hesam Mashhadi Mohammad <s271199@studenti.polito.it>
2021-12-08 22:13:30 +0330

    padding for the header(logo)

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,9 +3,9 @@ import Button from "react-bootstrap/Button";
 
 function Header(props) {
 	return (
-		<>
+		<div className="header">
 			<img id ='skeeep-logo' alt={"skeeep-logo"} src={"skeeep.svg"}/>
-		</>
+		</div>
 	);
 }
 
