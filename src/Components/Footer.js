a1579da6227b9548f1617f264d7a4f8b14a8e1be
e-caste-mbp <git@caste.dev>
2021-12-09 23:20:13 +0100

    make proper footer component. ref #28

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,10 +1,20 @@
 import React from "react";
+import {Image} from "react-bootstrap";
 import translations from "./Translations";
+import "./Footer.css";
 
 function Footer(props) {
 	return ( 
 		<>
-			<h3>{translations[props.lang].footer}</h3>
+			<h5>
+				<a href={"https://github.com/weee-open/skeeep"}>
+					<Image id={"footer-skeeep-logo"} src={"skeeep.svg"} alt={"skeeep"} />
+				</a>
+				{translations[props.lang].footer}
+				<a href={"https://weeeopen.polito.it"}>
+					<Image id={"footer-weee-open-logo"} src={"weee-open.svg"} alt={"WEEE Open"} />
+				</a>
+			</h5>
 		</>
 	);
 }
