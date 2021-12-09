180ed6370e441de5052df1feccfee6b69822cac4
e-caste-mbp <git@caste.dev>
2021-12-09 12:36:22 +0100

    fix App.js warnings

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import Button from "react-bootstrap/Button";
-import {useState, useEffect} from "react";
+import React, {useState, useEffect} from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import CardsContainer from "./Components/CardsContainer";
