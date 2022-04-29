5ddd7064f39dcd840736cc1e78ea7273c6aff48e
e-caste-mbp <git@caste.dev>
2022-04-29 11:35:49 +0200

    push newly added cards to top of page for a 30 days grace period. ref #46

--- src/Components/CardsContainer.js
+++ src/Components/CardsContainer.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import SingleCard from "./SingleCard";
 import { Container, Row, Col } from "react-bootstrap";
 import "./CardsContainer.css";
@@ -9,38 +9,59 @@ function CardsContainer(props) {
       name: "politools",
       title: "PoliTools",
       url: "https://chrome.google.com/webstore/detail/politools/fbbjhoaakfhbggkegckmjafkffaofnkd",
+      addedDate: new Date("2022-03-11"),
     },
     {
       name: "inginfbot",
       title: "inginf_bot",
       url: "https://t.me/inginf_bot",
+      addedDate: new Date("2022-03-11"),
     },
     {
       name: "studyplanner",
       title: "Study Planner",
       url: "https://github.com/e-caste/study-planner",
+      addedDate: new Date("2022-03-11"),
     },
     {
       name: "politogruppibot",
       title: "PoliToGruppiBot",
       url: "https://t.me/politogruppibot",
+      addedDate: new Date("2022-03-11"),
     },
     {
       name: "sonousciteledate",
       title: "sonousciteledate.it",
       url: "https://sonousciteledate.it",
+      addedDate: new Date("2022-03-11"),
     },
     {
       name: "politomateriale",
       title: "polito-materiale",
       url: "https://github.com/lucaceriani/polito-materiale",
+      addedDate: new Date("2022-03-11"),
     },
     {
       name: "skeeelled",
       title: "skeeelled",
       url: "#",
+      addedDate: new Date("2022-03-11"),
     },
   ]);
+
+  const gracePeriod = 30; // days that newly added cards will be on top
+
+  useEffect(() => {
+    let sortedCards = [];
+    cards.forEach((card) => {
+      if (new Date() - card.addedDate < gracePeriod * 24 * 60 * 60 * 1000) {
+        sortedCards.unshift(card); // add at the beginning
+      } else {
+        sortedCards.push(card); // add at the end
+      }
+    });
+    setCards(sortedCards);
+  }, []);
 
   return (
     <Container className="cardsContainer">
